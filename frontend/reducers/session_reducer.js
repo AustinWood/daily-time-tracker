66d27cc3660e895cf00eb72939053762f0224114
Austin Wood <austin@austinkwood.com>
2017-07-14 14:54:26 -0700

    Receive current user on successful signup

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -1,7 +1,7 @@
-// import {
-//   RECEIVE_CURRENT_USER,
-//   LOGOUT,
-//   RECEIVE_ERRORS } from '../actions/session_actions';
+import {
+  RECEIVE_CURRENT_USER,
+  LOGOUT,
+  RECEIVE_ERRORS } from '../actions/session_actions';
 import merge from 'lodash/merge';
 
 const _nullUser = Object.freeze({
@@ -12,16 +12,16 @@ const _nullUser = Object.freeze({
 const SessionReducer = (state = _nullUser, action) => {
   Object.freeze(state);
   switch(action.type) {
-    // case RECEIVE_CURRENT_USER:
-    //   const currentUser = action.currentUser;
-    //   return merge({}, _nullUser, { currentUser });
-    // case LOGOUT:
-    //   return merge({}, _nullUser);
-    // case RECEIVE_ERRORS:
-    //   const errors = action.errors;
-    //   return merge({}, _nullUser, {
-    //     errors
-    //   });
+    case RECEIVE_CURRENT_USER:
+      const currentUser = action.currentUser;
+      return merge({}, _nullUser, { currentUser });
+    case LOGOUT:
+      return merge({}, _nullUser);
+    case RECEIVE_ERRORS:
+      const errors = action.errors;
+      return merge({}, _nullUser, {
+        errors
+      });
     default:
       return state;
   }
