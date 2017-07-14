f644cac5a560aaaaf6b1ddbced3b9c9c5c37e347
Austin Wood <austin@austinkwood.com>
2017-07-14 14:13:38 -0700

    Redirect if logged in

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 
-const SessionForm = () => (
-  <div className="session-form">
-    <p>Login in / Create account</p>
-  </div>
-);
+class SessionForm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentWillMount() {
+    this.redirectIfLoggedIn()
+  }
+
+  redirectIfLoggedIn() {
+    if (this.props.loggedIn) {
+      this.props.history.push('/');
+    }
+  };
+
+  ensureLoggedIn() {
+
+  };
+
+  render() {
+    return (
+      <div className="session-form">
+        <p>Login in / Create account</p>
+      </div>
+    );
+  }
+}
 
 export default SessionForm;
